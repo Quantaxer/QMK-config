38987b23a51eb8286071e520f5b82ae14c1ec8e8
Phudel <phudel@uoguelph.ca>
2020-02-09 13:50:07 -0500

    test

--- keyboards/massdrop/alt/keymaps/custom/rgb_matrix_user/custom.h
+++ keyboards/massdrop/alt/keymaps/custom/rgb_matrix_user/custom.h
@@ -15,7 +15,7 @@ static HSV math(HSV hsv,  int16_t dx, int16_t dy, uint8_t dist, uint16_t tick) {
 
 #ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
 bool custom(effect_params_t* params) {
-    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+    /*RGB_MATRIX_USE_LIMITS(led_min, led_max);
     for(int i=0; i<=led_max; i++) {
         if (i < led_max-18 || i > led_max-6) {
           rgb_matrix_set_color(i, 44, 90, 255);
@@ -23,7 +23,7 @@ bool custom(effect_params_t* params) {
         else {
           rgb_matrix_set_color(i, 0, 0, 0);
         }
-    }
+    }*/
     return effect_runner_reactive_splash(qsub8(g_last_hit_tracker.count, 1), params, &math);
 }
 
--- quantum/version.h
+++ quantum/version.h
@@ -1,2 +1,2 @@
-#define QMK_VERSION "9ae1c6-dirty"
-#define QMK_BUILDDATE "2020-02-09-13:46:59"
+#define QMK_VERSION "43190b-dirty"
+#define QMK_BUILDDATE "2020-02-09-13:49:49"
