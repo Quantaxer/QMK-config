ae89204bea2fd86f92152661550445eec61def59
Phudel <phudel@uoguelph.ca>
2020-02-09 15:28:02 -0500

    Edit custom effect

--- keyboards/massdrop/alt/keymaps/custom/rgb_matrix_user/custom.h
+++ keyboards/massdrop/alt/keymaps/custom/rgb_matrix_user/custom.h
@@ -3,17 +3,14 @@ RGB_MATRIX_EFFECT(custom)
 
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-static HSV math(HSV hsv,  int16_t dx, int16_t dy, uint8_t dist, uint16_t tick) {
-    uint16_t effect = tick + dist * 5;
-    if (effect > 255)
-        effect = 255;
-    hsv.v = qadd8(hsv.v, 255 - effect);
+static HSV math(HSV hsv, uint16_t offset) {
+    hsv.v = scale8(255 - offset, hsv.v);
     return hsv;
 }
 
 #ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
 bool custom(effect_params_t* params) {
-    return effect_runner_reactive_custom(qsub8(g_last_hit_tracker.count, 1), params, &math);
+    return effect_runner_reactive_custom(params, &math);
 }
 
 #endif
--- quantum/version.h
+++ quantum/version.h
@@ -1,2 +1,2 @@
-#define QMK_VERSION "c3ca01-dirty"
-#define QMK_BUILDDATE "2020-02-09-15:13:39"
+#define QMK_VERSION "de360e-dirty"
+#define QMK_BUILDDATE "2020-02-09-15:27:41"
