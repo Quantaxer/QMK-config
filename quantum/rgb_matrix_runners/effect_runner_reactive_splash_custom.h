c6884787cd3d9e968ca64e8f73cd556501a8ae02
Phudel <phudel@uoguelph.ca>
2020-02-10 18:02:57 -0500

    test

--- quantum/rgb_matrix_runners/effect_runner_reactive_splash_custom.h
+++ quantum/rgb_matrix_runners/effect_runner_reactive_splash_custom.h
@@ -28,7 +28,7 @@ bool effect_runner_reactive_custom(effect_params_t* params, reactive_f effect_fu
     
     //hsv2.h = abs8(25 * sin8(globalCounter / 100000)) + rgb_matrix_config.hsv.h;
     float newCount = globalCounter * 180/3.14;
-    hsv2.h = (dist / 4) + abs(70 * sin(newCount / 30000 * (rgb_matrix_config.speed / 20))) - rgb_matrix_config.hsv.h;
+    hsv2.h = (dist / 5) + abs(40 * sin(newCount / 10000)) - rgb_matrix_config.hsv.h;
     //ceil((25 * sin(count / 1000) + (dist / 2)) + (rgb_matrix_config.hsv.h + 25));
 
     if (tick != max_tick) {
--- quantum/version.h
+++ quantum/version.h
@@ -1,2 +1,2 @@
-#define QMK_VERSION "74fac5-dirty"
-#define QMK_BUILDDATE "2020-02-10-17:59:27"
+#define QMK_VERSION "bb5949-dirty"
+#define QMK_BUILDDATE "2020-02-10-18:02:53"
